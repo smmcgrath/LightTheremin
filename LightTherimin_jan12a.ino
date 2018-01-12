276d4701e015c3779b37bd0ae6669cb06dcace7e
Shane Michael McGrath <30355322+smmcgrath@users.noreply.github.com>
2018-01-12 15:12:06 +0000

    LIghtTheremin
    Building a Light Theremin musical instrument with the Arduino board.
    
    A phototransistor is used to measure the amount of light reaching the instrument. This is used to estimate how close the musicians hand is to the instrument at a given moment. More specifically the variation of voltage is used to guage the distance. This is then mapped to a range of sound frequencies. A piezo buzzer is used to produce the sound.
    
    The Arduino sketch is written in a language which shares similarities with C and C++.

---
+++ LightTherimin_jan12a.ino
@@ -0,0 +1,50 @@
+// stores the values read from light sensor
+int sensorValue;
+// min and max values used for calibration
+int sensorLow = 1023;
+int sensorHigh = 0;
+
+// constant, using onboard LED on pin 13
+const int ledPin = 13;
+
+void setup() {
+  // turn on LED to signal the beginning of calibration
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
+
+  // calibrate during the first 5 seconds
+  while (millis() < 5000){
+    sensorValue = analogRead(A0);
+
+    // record the maximum sensor value
+    if (sensorValue > sensorHigh){
+      sensorHigh = sensorValue;
+    }
+
+    // record the minimum sensor value
+    if (sensorValue < sensorLow){
+      sensorLow = sensorValue;  
+    }
+  }//end while loop
+
+  // signal the end of calibration period
+  digitalWrite(ledPin, LOW);
+}//end setup
+
+void loop() {
+  // read the input from A0
+  sensorValue = analogRead(A0);
+  
+  // map the sensor values to  a wide range of pitches
+  // adjust the values below to conform to the maximum
+  // and minimum values from the sensor
+  int pitch = map(sensorValue, sensorLow, sensorHigh, 50, 4000);
+
+  // play the tone
+  // in the format (pin, pitch, duration)
+  tone(8, pitch, 20);
+
+  // wait for period
+  delay(10);
+  
+}//end loop
